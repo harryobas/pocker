4c9eaf3d03c60489cba6788c7b27647ef166077d
Harry Obaseki <harryobas@gmail.com>
2019-06-14 11:42:26 +0200

    implemented rank computation for straight flush

--- lib/rank_calculator.ex
+++ lib/rank_calculator.ex
@@ -4,38 +4,83 @@ defmodule PockerOpdracht.RankCalculator do
   provides functions cumputing the rank of a pocker hand
   """
 
-  @spec compute_rank([{atom, String.t}]) :: {:ok, String.t, atom}
+  @index [0, 1, 2, 3, 4]
+  @values_rank %{
+    "2" => 0,
+    "3" => 1,
+    "4" => 2,
+    "5" => 3,
+    "6" => 4,
+    "7" => 5,
+    "8" => 6,
+    "9" => 7,
+    "T" => 8,
+    "J" => 9,
+    "Q" => 10,
+    "K" => 11,
+    "A" => 12
+  }
+
+
   def compute_rank(pocker_hand) do
-    hand_name = pocker_hand |> Enum.into(%{}) |> Map.keys |> List.first |> to_string
+    hand_name =
+      pocker_hand
+      |> Enum.into(%{})
+      |> Map.keys
+      |> List.first
+      |> to_string
+
     poker_hand = hd(pocker_hand)
     {_, deck} = poker_hand
     suits = deck |> String.split(" ") |> extract_suites
-    case is_straight_flush?(suits) do
+
+    values = deck |> String.split(" ") |> extract_values
+
+
+    case is_straight_flush?(suits, values) do
       true -> {:ok, hand_name, :straight_flush}
-      _ -> :nomatch 
+      false -> :nomatch
     end
   end
 
-    defp extract_suites([head|tail]) do
-      extracted = []
-      suite_element = head |> String.split("") |> Enum.at(2)
-      extracted = extracted ++ [suite_element]
-      extract_suites(tail)
-      extracted
+    defp extract_suites(suits) do
+      Enum.map suits, fn s ->
+        s |> String.split("") |> Enum.at(2)
+      end 
     end
 
-    defp extract_suites([]) do
+    defp extract_values(values) do
+      Enum.map values, fn v ->
+        v |> String.split("") |> Enum.at(1)
+      end
     end
 
-    defp is_straight_flush?(suite) when is_list(suite) do
-      Enum.all?(suite, fn s -> s == hd(suite) end)
+    defp is_straight_flush?(suits, values) when is_list(suits) do
+      case same_suits?(suits) && has_consecutive_values?(values) do
+        true -> true
+        false -> false
+      end
     end
 
-    defp is_straight_flush?([]) do
-      false
+    def same_suits?(suits) do
+      case Enum.count(suits) > 0 do
+        true -> Enum.all?(suits, fn s -> suits -- List.duplicate(s, 5) == [] end)
+        false -> false
+      end
     end
 
+    defp has_consecutive_values?(values) do
 
-
+      case Enum.count(values) > 0 do
+        false -> false
+        true ->
+          values = Enum.map values, fn x -> @values_rank[x] end
+          Enum.all? values, fn v ->
+            Enum.each @index, fn i ->
+              v + 1 == Enum.at(values, i + 1)
+            end
+          end
+        end
+    end
 
   end
--- test/rank_calculator_test.exs
+++ test/rank_calculator_test.exs
@@ -4,18 +4,11 @@ defmodule RankCalculatorTest do
 
   @subject PockerOpdracht.RankCalculator
 
-  describe "when 5 cards of the same suit with consecutive values" do
-    test "compute_rank/1 returns {:ok, hand_name :straight_flush}" do
-      hand = [black: "2H 3H 4H 5D 6H"]
+  describe "rank computation" do
+    test "5 cards of same suit with consecutive values - compute_rank/1 returns {:ok, hand_name :straight_flush}" do
+      hand = [black: "2H 3H 4H 5H 6H"]
       hand_name = hand |> Enum.into(%{}) |> Map.keys |> List.first |> to_string
       assert {:ok, hand_name, :straight_flush} == @subject.compute_rank(hand)
-    end
-  end
-  describe "when 4 cards have the same value" do
-    test "compute_rank/1 returns {:ok, hand_name, :four_of_a_kind}" do
-      hand = [white: "3C 3H 3S 8C 3H"]
-      hand_name = hand |> Enum.into(%{}) |> Map.keys |> List.first |> to_string
-      assert {:ok, hand_name, :four_of_a_kind} == @subject.compute_rank(hand)
     end
   end
 
