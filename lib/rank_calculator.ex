64b4499d90a7a3979528c9c6425d4b191599b5fb
Harry Obaseki <harryobas@gmail.com>
2019-06-14 14:10:25 +0200

    implemented rank computation for four of a kind

--- lib/rank_calculator.ex
+++ lib/rank_calculator.ex
@@ -21,7 +21,6 @@ defmodule PockerOpdracht.RankCalculator do
     "A" => 12
   }
 
-
   def compute_rank(pocker_hand) do
     hand_name =
       pocker_hand
@@ -33,54 +32,77 @@ defmodule PockerOpdracht.RankCalculator do
     poker_hand = hd(pocker_hand)
     {_, deck} = poker_hand
     suits = deck |> String.split(" ") |> extract_suites
-
     values = deck |> String.split(" ") |> extract_values
 
-
-    case is_straight_flush?(suits, values) do
-      true -> {:ok, hand_name, :straight_flush}
-      false -> :nomatch
-    end
-  end
-
-    defp extract_suites(suits) do
-      Enum.map suits, fn s ->
-        s |> String.split("") |> Enum.at(2)
-      end 
-    end
-
-    defp extract_values(values) do
-      Enum.map values, fn v ->
-        v |> String.split("") |> Enum.at(1)
-      end
-    end
-
-    defp is_straight_flush?(suits, values) when is_list(suits) do
-      case same_suits?(suits) && has_consecutive_values?(values) do
-        true -> true
-        false -> false
-      end
-    end
-
-    def same_suits?(suits) do
-      case Enum.count(suits) > 0 do
-        true -> Enum.all?(suits, fn s -> suits -- List.duplicate(s, 5) == [] end)
-        false -> false
-      end
-    end
-
-    defp has_consecutive_values?(values) do
-
-      case Enum.count(values) > 0 do
-        false -> false
-        true ->
-          values = Enum.map values, fn x -> @values_rank[x] end
-          Enum.all? values, fn v ->
-            Enum.each @index, fn i ->
-              v + 1 == Enum.at(values, i + 1)
-            end
-          end
+    case get_rank(suits, values) do
+      {:ok, rank} ->
+        cond do
+          rank == :straight_flush -> {hand_name, :straight_flush, values}
+          rank == :four_of_a_kind -> {hand_name, :four_of_a_kind, values}
         end
     end
 
   end
+
+  defp get_rank(suits, values) do
+    case is_straight_flush?(suits, values) do
+      true -> {:ok, :straight_flush}
+      false ->
+        case is_four_of_a_kind?(values) do
+          true -> {:ok, :four_of_a_kind}
+          false -> :wrong
+        end
+    end
+
+  end
+
+  defp extract_suites(suits) do
+    Enum.map suits, fn s ->
+      s |> String.split("") |> Enum.at(2)
+    end
+  end
+
+  defp extract_values(values) do
+    Enum.map values, fn v ->
+      v |> String.split("") |> Enum.at(1)
+    end
+  end
+
+  defp is_straight_flush?(suits, values) when is_list(suits) do
+    case same_suits?(suits) && has_consecutive_values?(values) do
+      true -> true
+      false -> false
+    end
+  end
+
+  def same_suits?(suits) do
+    case Enum.count(suits) == 5 do
+      true -> Enum.all?(suits, fn s -> suits -- List.duplicate(s, 5) == [] end)
+      false -> false
+    end
+  end
+
+  defp has_consecutive_values?(values) do
+    case Enum.count(values) == 5 do
+      false -> false
+      true ->
+        values = Enum.map values, fn x -> @values_rank[x] end
+        Enum.all? values, fn v ->
+          Enum.each @index, fn i ->
+            v + 1 == Enum.at(values, i + 1)
+          end
+        end
+      end
+  end
+
+  defp is_four_of_a_kind?(values) do
+    case Enum.count(values) == 5 do
+      false -> false
+      true ->
+        values = Enum.map values, fn x -> @values_rank[x] end
+        values |> Enum.uniq() |> Enum.count() == 2
+      end
+    end
+
+  
+end
--- test/rank_calculator_test.exs
+++ test/rank_calculator_test.exs
@@ -7,8 +7,15 @@ defmodule RankCalculatorTest do
   describe "rank computation" do
     test "5 cards of same suit with consecutive values - compute_rank/1 returns {:ok, hand_name :straight_flush}" do
       hand = [black: "2H 3H 4H 5H 6H"]
+      values = ["2", "3", "4", "5", "6"]
       hand_name = hand |> Enum.into(%{}) |> Map.keys |> List.first |> to_string
-      assert {:ok, hand_name, :straight_flush} == @subject.compute_rank(hand)
+      assert {hand_name, :straight_flush, values} == @subject.compute_rank(hand)
+    end
+    test "4 cards with the same value - compute_rank/1 returns {:ok, hand_name, :four_of_a_kind}" do
+      hand = [white: "3H 5D 5S 5C 5H"]
+      values = ["3", "5", "5", "5", "5"]
+      hand_name = hand |> Enum.into(%{}) |> Map.keys |> List.first |> to_string
+      assert {hand_name, :four_of_a_kind, values} == @subject.compute_rank(hand)
     end
   end
 
